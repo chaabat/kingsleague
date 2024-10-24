f82a41ff261ea8459b2196ed795e65f0f1a6fa31
Ayoub CHAABAT <chaabat.02@gmail.com>
2024-10-24 09:34:23 +0100

    Player Service

--- src/main/java/com/kingsleague/service/PlayerService.java
+++ src/main/java/com/kingsleague/service/PlayerService.java
@@ -1,8 +1,11 @@
 package com.kingsleague.service;
 
 
+import com.kingsleague.model.Player;
 import com.kingsleague.repository.interfaces.PlayerRepository;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Transactional
 public class PlayerService {
@@ -10,5 +13,29 @@ public class PlayerService {
 
     public void setPlayerRepository(PlayerRepository playerRepository) {
         this.playerRepository = playerRepository;
+    }
+
+    public Player getPlayerById(Long id) {
+        return playerRepository.get(id);
+    }
+
+    @Transactional(readOnly = true)
+    public List<Player> getAllPlayers() {
+        return playerRepository.getAll();
+    }
+
+    public Player getPlayerByUsername(String username) {
+        return playerRepository.getByUsername(username);
+    }
+
+    public void addPlayer(Player player) {
+        playerRepository.add(player);
+    }
+
+    public void updatePlayer(Player player) {
+        playerRepository.update(player);
+    }
+    public void deletePlayer(Long id) {
+        playerRepository.delete(id);
     }
 }
