2effebaa6228cd591bfa62f52f6f21e1a4acfe24
Ayoub CHAABAT <chaabat.02@gmail.com>
2024-10-24 09:39:13 +0100

    Team Service

--- src/main/java/com/kingsleague/service/TeamService.java
+++ src/main/java/com/kingsleague/service/TeamService.java
@@ -1,9 +1,12 @@
 package com.kingsleague.service;
 
 
+import com.kingsleague.model.Team;
 import com.kingsleague.repository.interfaces.PlayerRepository;
 import com.kingsleague.repository.interfaces.TeamRepository;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Transactional
 public class TeamService {
@@ -16,5 +19,29 @@ public class TeamService {
 
     public void setPlayerRepository(PlayerRepository playerRepository) {
         this.playerRepository = playerRepository;
+    }
+
+    public Team getTeamById(Long id) {
+        return teamRepository.get(id);
+    }
+
+    @Transactional(readOnly = true)
+    public List<Team> getAllTeams() {
+        return teamRepository.getAll();
+    }
+
+    public Team getTeamByName(String name) {
+        return teamRepository.getByName(name);
+    }
+
+    public void addTeam(Team team) {
+        teamRepository.add(team);
+    }
+    public void updateTeam(Team team) {
+        teamRepository.update(team);
+    }
+    public void deleteTeam(Long id ) {
+        teamRepository.delete(id);
+
     }
 }
