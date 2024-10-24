574e24cf31e2d52a7f17b83632cc3e255bffe8b0
Ayoub CHAABAT <chaabat.02@gmail.com>
2024-10-24 09:17:10 +0100

    Game Service

--- src/main/java/com/kingsleague/service/GameService.java
+++ src/main/java/com/kingsleague/service/GameService.java
@@ -1,8 +1,11 @@
 package com.kingsleague.service;
 
+import com.kingsleague.model.Game;
 import com.kingsleague.repository.interfaces.GameRepository;
 import com.kingsleague.repository.interfaces.TeamRepository;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Transactional
 public class GameService {
@@ -18,4 +21,31 @@ public class GameService {
     public void setTeamRepository(TeamRepository teamRepository) {
         this.teamRepository = teamRepository;
     }
+
+    public Game getGame(Long id) {
+        return gameRepository.get(id);
+    }
+
+    @Transactional(readOnly = true)
+    public List<Game> getAllGames() {
+        return gameRepository.getAll();
+    }
+
+    public Game getGameByName(String name){
+        return gameRepository.getByName(name);
+    }
+
+    public void addGame(Game game) {
+        gameRepository.add(game);
+    }
+
+    public void updateGame(Game game) {
+        gameRepository.update(game);
+    }
+
+    public void deleteGame(Long id) {
+        gameRepository.delete(id);
+    }
+
+
 }
